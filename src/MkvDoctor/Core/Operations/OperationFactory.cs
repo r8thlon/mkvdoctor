89bee0ce2f1b66c6823c1cf817ca1120010c4771
r8thlon <jdlozada@proton.me>
2026-09-19 12:56:48 -0400

    Add Mute Video operation and fix OperationType enum ordering

--- src/MkvDoctor/Core/Operations/OperationFactory.cs
+++ src/MkvDoctor/Core/Operations/OperationFactory.cs
@@ -5,8 +5,9 @@ namespace MkvDoctor.Core.Operations;
 public enum OperationType
 {
     FixDuration,
+    ConvertToMkv,
     Concatenate,
-    ConvertToMkv
+    MuteVideo
 }
 
 public static class OperationFactory
@@ -16,6 +17,7 @@ public static class OperationFactory
         OperationType.FixDuration => new FixDurationOperation(ffmpeg),
         OperationType.Concatenate => new ConcatenateOperation(ffmpeg),
         OperationType.ConvertToMkv => new ConvertToMkvOperation(ffmpeg),
+        OperationType.MuteVideo => new MuteVideoOperation(ffmpeg),
         _ => throw new ArgumentOutOfRangeException(nameof(type))
     };
 }
